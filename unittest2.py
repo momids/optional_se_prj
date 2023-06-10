365d8b43018bbe314f88b0b2447fe2169d4cdff0
Mohammad Omid Salehi <120191511+momids@users.noreply.github.com>
2023-06-10 21:35:31 +0330

    add simple PRJ and unittests

---
+++ unittest2.py
@@ -0,0 +1,20 @@
+import unittest
+from newmain import factorial
+
+
+class TestFactorial(unittest.TestCase):
+    def test_factorial_0(self):
+        self.assertEqual(factorial(0), 1)
+
+    def test_factorial_1(self):
+        self.assertEqual(factorial(1), 1)
+
+    def test_factorial_positive(self):
+        self.assertEqual(factorial(5), 120)
+
+    def test_factorial_large(self):
+        self.assertEqual(factorial(10), 3628800)
+
+
+if __name__ == '__main__':
+    unittest.main(module='test_factorial')
